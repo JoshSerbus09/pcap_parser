50cc5733eb33e569ac6c888edea1ed5f74307cdb
Josh Serbus <JoshSerbus09@gmail.com>
2013-04-16 20:31:35 -0500

    Parser grabs channels correctly. Most are 802.11 as expected

--- pcap_parser.rb
+++ pcap_parser.rb
@@ -5,6 +5,7 @@ def pcap_parse file
 	attributes = Hash.new
 	ssids = Hash.new
 	channels = Hash.new
+	unique_channels_count = 0
 	ssid_count = 0
 	unique_ssid_count = 0
 
@@ -17,32 +18,41 @@ def pcap_parse file
 
 			# Record all of the unique SSIDS, slicing the string to remove [Packet yadda yadda]
 			if item.include? 'SSID='
-				if ssids[item] == nil
-					if item.include? '['
-						ssids[item] = item.slice(3..(item.index('[') - 1))
-					else
-						ssids[item] = item
-					end		
-					unique_ssid_count += 1	
-				end
-				ssid_count += 1
+				if item.include? '['
+					ssids[item] = item.slice(3..(item.index('[') - 1))
+				else
+					ssids[item] = item
+				end		
+				unique_ssid_count += 1	
 			end
 
+			# Record all the unique channels. Most likely all going to be 802.11
+			if item.include? '802.11'
+				channels[item] = item
+				unique_channels_count += 1
+			end		
 		}
 		attributes["ssids"] = ssids
+		attributes["channels"] = channels
 	}
 
 	puts "=============================================="
 	puts "SSIDS found:"
 	puts "=============================================="
 	attributes["ssids"].each { |key, value|
-		puts "#{value}"		
+		puts "\t#{value}"		
 	}
+	puts "=============================================="
+	puts "Channels found:"
+	puts "=============================================="
+		attributes["channels"].each { |key, value| 
+			puts "\t#{value}"
+		}
 	puts "=============================================="
 
 	puts "Lines in CSV: " + file_line_count.to_s
-	puts "SSIDS found: " + ssid_count.to_s
 	puts "Unique SSIDS found: " + unique_ssid_count.to_s
+	puts "Unique channels found: " + unique_channels_count.to_s
 end	
 
 pcap_parse(File.open('pcap_CSV'))
